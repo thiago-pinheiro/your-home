08f3713fe1c6ec54e9d389d82c751ac8dbcef965
thiago-pinheiro <dev.thiagopinheiro@gmail.com>
2022-04-06 02:29:24 -0300

    feat: created the component file, helpers, page folders and adjusted the login page style

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,36 @@
 import './style.css'
+import logo from './your.png'
 
 function Login() {
   return (
-    <div className="titleLogin">
-      <h1>Your Home</h1>
+    <div className='divLogin'>
+
+      <div className='imageLogo'>
+        <img alt='logo do aplicativo' src={logo} width='150px' style={{ borderRadius: '100%' }}></img>
+      </div>
+
+      <div className='divTextField'>
+        <input className='loginInput' placeholder='usuário' />
+        <input className='loginInput' placeholder='senha' />
+      </div>
+
+      <div className='divHelp'>
+        <div className='divInputRemember'>
+          <input type='checkbox' className='inputRemember' />
+          <p className='labelRemember'>Lembrar usuário</p>
+        </div>
+        <a href='/' className='linkForgotPassword'>Esqueci minha senha</a>
+      </div>
+
+      <div className='divButton'>
+        <button className='buttonLogin'>Login</button>
+        <button className='buttonFirstAcess'>Primeiro Acesso ?</button>
+      </div>
+
+      <div className='footer'>
+        <a href='/' className='linkForgotPassword'>Termos de uso</a>
+      </div>
+
     </div>
   );
 }
