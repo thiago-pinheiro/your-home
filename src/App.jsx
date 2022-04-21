c433f95cfd830eef75349745fb96bc5e2115c1c2
thiago-pinheiro <dev.thiagopinheiro@gmail.com>
2022-04-21 16:03:39 -0300

    feat: started page creation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import CreateAccount from "./pages/createAccount";
+import CreateAd from "./pages/createAd";
+import Login from "./pages/login";
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      {/* <CreateAccount /> */}
+      {/* <CreateAd/> */}
+    </div>
+  );
+}
+
+export default App;
